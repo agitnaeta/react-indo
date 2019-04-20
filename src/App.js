cdbcc3a44294c8feb2fc2015bffb8d98830cdb28
root <root@gitserver.local>
2019-04-21 02:12:44 +0700

    Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -7,18 +7,44 @@ class App extends Component {
     super(props)
     this.state = {
       name : '',
+      stillType: '',
     }
-
+    
   }
 
   setName = (e) => {
     let name = e.target.value
     this.setState({
-      name : name
+      name : name,
+      stillType : true,
+    })
+  }
+  setStillType= () =>{
+    this.setState({
+      stillType : false
     })
   }
 
+  componentDidMount(){
+    this.setState({
+      stillType : false,
+    })
+  }
+
+  componentDidUpdate(){
+    console.log('component melakukan update')
+  }
   render() {
+    if(this.state.stillType===true){
+      return(
+        <div>
+          <h1>{this.state.name}</h1>
+          <input onChange ={this.setName}/>
+          <button onClick={this.setStillType}> submit</button>
+          <p> Loading ... </p>
+        </div>
+      )
+    }
     return (
       <div>
          <h1>{this.state.name}</h1>
